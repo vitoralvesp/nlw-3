d925e538861b6b72a10ca0eaa43eac2f6a637ce5
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2020-10-14 18:10:06 -0300

    :octocat:

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,71 @@
+// create map
+const map = L.map("mapid").setView([-27.2221491, -49.649536], 15);
+
+// create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+});
+
+let marker;
+
+// create and add marker
+map.on("click", (event) => {
+  const lat = event.latIng.lat;
+  const lng = event.latIng.lng;
+
+  document.querySelector("[name=lat]"), (value = lat);
+  document.querySelector("[name=lng]"), (value = lng);
+
+  //remove icon
+  marker && map.removeLayer(marker);
+
+  //add icon layer
+  marker = L.marker([lat, lng], { icon }).addTo(map);
+});
+
+// add photos
+function addPhotoField() {
+  //pegar container de fotos #images
+  const container = document.querySelector("#images");
+
+  //pegar o container para duplicar .new-image
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+
+  //realizar o clone da última imagem adicionada
+  const newFieldContainer = fieldsContainer[
+    fieldsContainer.length - 1
+  ].cloneNode(true);
+
+  //verificar se o campo está vazio
+  const input = newFieldContainer.children[0];
+
+  if (input.value == "") {
+    return;
+  }
+
+  // limpar o campo antes de add
+  input.value = "";
+
+  // add o clone ao container de #images
+  container.appendChild(newFieldContainer);
+}
+
+function deleteField(event) {
+  const span = event.currentTarget;
+
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+
+  if (fieldsContainer.length < 2) {
+    //limpar o valor do campo
+    span.parentNode.children[0].value = "";
+    return;
+  }
+
+  // deletar o campo
+  span.parentNode.remove();
+}
---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,47 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
+
+// create map
+const map = L.map("mapid", options).setView([-27.2221491, -49.649536], 15);
+
+// create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// create and add marker
+L.marker([-27.2221491, -49.649536], { icon }).addTo(map);
+
+// image gallery
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  // remover todas as classes .active
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
+
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
+
+  //selecionar a imagem clicada
+  const image = button.children[0];
+  const imageContainer = document.querySelector("orphanage-details > img");
+
+  //atualizar o container de imagem
+  imageContainer.src = image.src;
+
+  //adicionar a classe .active para este botão
+  button.classList.add("active");
+}
