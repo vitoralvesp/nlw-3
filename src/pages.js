fa14a562ebefa1e013ad0a47bdc3a7cde722a7c0
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2020-10-16 16:50:53 -0300

    :octocat:

--- src/pages.js
+++ src/pages.js
@@ -1,21 +1,82 @@
-const orphanages = require('./database/fakedata.js')
+//const {catch}  = require('./database/db');
+const Database = require("./database/db");
+const saveOrphanage = require("./database/saveOrphanage");
 
 module.exports = {
-    
-    index(req, res) {
-        return res.render('index')
-    },
+  index(req, res) {
+    return res.render("index");
+  },
 
-    orphanage(req, res) {
-        return res.render('orphanage')
-    },
+  async orphanage(req, res) {
+    const id = req.query.id;
+    try {
+      const db = await Database;
 
-    orphanages(req, res) {
-        return res.render('orphanages', { orphanages })
-    },
+      const results = await db.all(
+        `SELECT * FROM orphanages WHERE id = "${id}"`
+      );
+      const orphanage = results[0];
 
-    createOrphanage(req, res) {
-        return res.render('create-orphanage')
-    },
-    
-}
+      orphanage.images = orphanage.images.split(",");
+      orphanage.firstImage = orphanage.images[0];
+
+      if (orphanage.open_on_weekends == 0) {
+        orphanage.open_on_weekends = false;
+      } else {
+        orphanage.open_on_weekends = true;
+      }
+
+      return res.render("orphanage", { orphanage });
+    } catch (error) {
+      console.log(error);
+      return res.send("Erro no banco de dados...");
+    }
+  },
+
+  async orphanages(req, res) {
+    try {
+      const db = await Database;
+      const orphanages = await db.all("SELECT * FROM orphanages");
+      return res.render("orphanages", { orphanages });
+    } catch (error) {
+      console.log(error);
+      return res.send("Erro no banco de dados...");
+    }
+  },
+
+  createOrphanage(req, res) {
+    return res.render("create-orphanage");
+  },
+
+  async saveOrphanage(req, res) {
+    const fields = req.body;
+
+    //validar se todos os campos estão preenchidos
+    if (Object.values(fields).includes("")) {
+      return res.send("Todos os campos devem ser preenchidos");
+    }
+
+    try {
+      //salvar um orfanato
+      const db = await Database;
+      await saveOrphanage(db, {
+        lat: fields.lat,
+        lng: fields.lng,
+        name: fields.name,
+        about: fields.about,
+        whatsapp: fields.whatsapp,
+        images: fields.images.toString(),
+        instructions: fields.instructions,
+        opening_hours: fields.opening_hours,
+        open_on_weekends: fields.open_on_weekends,
+      });
+
+      //redirecionamento
+      return res.redirect('/orphanages')
+      
+    } catch (error) {
+      console.log(error);
+      return res.rend("Erro no banco de dados...");
+    }
+  },
+};
--- src/server.js
+++ src/server.js
@@ -6,18 +6,23 @@ const pages = require('./pages');
 //iniciando o express
 const server = express()
 server
+
+//utilizar body do req
+.use(express.urlencoded({ extended: true}))
+
 //utilizando os arquivos estáticos
-.use(express.static('public'))
+    .use(express.static('public'))
 
 //configurar template engine
-.set('views', path.join(__dirname, "views"))
-.set('view engine', 'hbs')
+    .set('views', path.join(__dirname, "views"))
+    .set('view engine', 'hbs')
 
 // rotas da aplicação
-.get('/', pages.index)
-.get('/orphanage', pages.orphanage)
-.get('/orphanages', pages.orphanages)
-.get('/create-orphanage', pages.createOrphanage)
+    .get('/', pages.index)
+    .get('/orphanage', pages.orphanage)
+    .get('/orphanages', pages.orphanages)
+    .get('/create-orphanage', pages.createOrphanage)
+    .post('/save-orphanage', pages.saveOrphanage)
 
 //ligar o servidor
 server.listen(5500)
